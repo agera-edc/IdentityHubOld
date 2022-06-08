7efedea9e4f0a852a384f09c9e351265df3cdbd4
Marc Gomez <marcgomez@microsoft.com>
2022-06-08 17:50:18 +0200

    Import cleanup

--- extensions/identity-hub/src/test/java/org/eclipse/dataspaceconnector/identityhub/api/featuredetection/IdentityHubControllerTest.java
+++ extensions/identity-hub/src/test/java/org/eclipse/dataspaceconnector/identityhub/api/featuredetection/IdentityHubControllerTest.java
@@ -1,22 +1,17 @@
 package org.eclipse.dataspaceconnector.identityhub.api.featuredetection;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.specification.RequestSpecification;
 import org.eclipse.dataspaceconnector.dtos.RequestObject;
 import org.eclipse.dataspaceconnector.identityhub.client.ApiClient;
 import org.eclipse.dataspaceconnector.identityhub.client.ApiClientFactory;
 import org.eclipse.dataspaceconnector.junit.launcher.EdcExtension;
-import org.eclipse.dataspaceconnector.junit.launcher.EdcRuntimeExtension;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @ExtendWith(EdcExtension.class)
 public class IdentityHubControllerTest {
@@ -34,7 +29,7 @@ public class IdentityHubControllerTest {
             .post()
         .then()
             .statusCode(200)
-            .body("requestId", Matchers.equalTo(requestId));
+            .body("requestId", equalTo(requestId));
     }
 
     private RequestSpecification baseRequest() {
