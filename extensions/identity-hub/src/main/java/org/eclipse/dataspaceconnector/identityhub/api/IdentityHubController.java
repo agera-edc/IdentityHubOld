411f6bd0db18a022303ec22a5472a73dc76b2d9b
Marc Gomez <marcgomez@microsoft.com>
2022-06-08 17:55:29 +0200

    Prevent using new in constructor for testability

--- extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/identityhub/api/IdentityHubController.java
+++ extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/identityhub/api/IdentityHubController.java
@@ -12,7 +12,6 @@ import org.eclipse.dataspaceconnector.dtos.ResponseObject;
 import org.eclipse.dataspaceconnector.dtos.RequestStatus;
 import org.eclipse.dataspaceconnector.service.MethodProcessor;
 import org.eclipse.dataspaceconnector.service.MethodProcessorFactory;
-import org.eclipse.dataspaceconnector.store.IdentityHubStore;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,12 +22,10 @@ import java.util.stream.Collectors;
 @Path("/identity-hub")
 public class IdentityHubController {
 
-    private IdentityHubStore identityHubStore;
     private final MethodProcessorFactory methodProcessorFactory;
 
-    public IdentityHubController(IdentityHubStore identityHubStore) {
-        this.identityHubStore = identityHubStore;
-        this.methodProcessorFactory = new MethodProcessorFactory(identityHubStore);
+    public IdentityHubController(MethodProcessorFactory methodProcessorFactory) {
+        this.methodProcessorFactory = methodProcessorFactory;
     }
 
     @POST
