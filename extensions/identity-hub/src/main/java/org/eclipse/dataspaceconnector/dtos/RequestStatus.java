eb03b94ce16bb505e1a5c59988386e54c37d4974
Ophelie Le Mentec <17216799+ouphi@users.noreply.github.com>
2022-06-09 16:28:27 +0200

    Added TODOs.

--- extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/dtos/RequestStatus.java
+++ extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/dtos/RequestStatus.java
@@ -6,6 +6,7 @@ package org.eclipse.dataspaceconnector.dtos;
  */
 public class RequestStatus extends Status {
     public static final RequestStatus OK = new RequestStatus(200, "The request was successfully processed");
+    // TODO: Implement logic + tests for DID_NOT_FOUND and ERROR.
     public static final RequestStatus DID_NOT_FOUND = new RequestStatus(404, "Target DID not found within the Decentralized Web Node");
     public static final RequestStatus ERROR = new RequestStatus(500, "The request could not be processed correctly");
 
