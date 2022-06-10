e0ecb5b4819a8ba100fecac20a1e3004f4df0595
Marc Gomez <marcgomez@microsoft.com>
2022-06-10 13:37:43 +0200

    Improve Javadoc

--- extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/identityhub/processor/MessageProcessor.java
+++ extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/identityhub/processor/MessageProcessor.java
@@ -4,19 +4,24 @@ import org.eclipse.dataspaceconnector.identityhub.dtos.MessageResponseObject;
 import org.eclipse.dataspaceconnector.identityhub.dtos.WebNodeInterfaces;
 
 /**
+ * <p>
  * Each implementor of the MessageProcessor interface handles a message of a different Decentralized Web Node Interface method.
- * See https://identity.foundation/decentralized-web-node/spec/#interfaces for a list of interfaces available.
- *
+ * Refer to <a href="https://identity.foundation/decentralized-web-node/spec/#interfaces">the spec</a> for a list of interfaces available.
+ * </p>
+ * <p>
  * Messages may or may not contain additional data associated with it (when data is desired or required to be present for a given method invocation).
  * The MessageProcessor gets handed over this data in case it is available, or null otherwise.
- *
+ * </p>
+ * <p>
  * See {@link WebNodeInterfaces} for a list of currently supported interfaces. Currently the only supported interface that accepts data is "CollectionsWrite".
+ * </p>
  */
 public interface MessageProcessor {
 
     /**
-     * Processes the corresponding message
-     * @param data Optional byte array representing the message data
+     * Processes a message
+     *
+     * @param data Byte array representing the message data or null if no data available
      * @return MessageResponseObject
      */
     MessageResponseObject process(byte[] data);
